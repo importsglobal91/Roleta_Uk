2b6dcca972b6bebdd4be433034d4a3b60b8e7814
importsglobal91 <importsglobal91@gmail.com>
2026-03-22 20:09:36 +0000

    Update Roleta_Uk_MultiTable.py

--- Roleta_Uk_MultiTable.py
+++ Roleta_Uk_MultiTable.py
@@ -1,17 +1,1 @@
-ROLETAS_LINKS = {
-    "32Red Roulette": "https://www.32red.com/play/32red-roulette#playforreal",
-    "Dynasty Lightning Roulette": "https://www.32red.com/play/dynasty-lightning-roulette#playforreal",
-    "Lightning Roulette": "https://www.32red.com/play/lightning-roulette#playforreal",
-    "Red Door Roulette": "https://www.32red.com/play/red-door-roulette#playforreal",
-    "Diamond Rush Roulette": "https://www.32red.com/play/diamond-rush-roulette#playforreal",
-    "Live Roulette": "https://www.32red.com/play/live-roulette#playforreal",
-    "Power Up Roulette": "https://www.32red.com/play/power-up-roulette#playforreal",
-    "Fortune Roulette": "https://www.32red.com/play/fortune-roulette#playforreal",
-    "Fireball Roulette": "https://www.32red.com/play/fireball-roulette#playforreal",
-    "Mega Roulette 3000": "https://www.32red.com/play/mega-roulette-3000#playforreal",
-    "Gold Vault Roulette": "https://www.32red.com/play/gold-vault-roulette#playforreal",
-    "Lucky 6 Roulette": "https://www.32red.com/play/lucky-6-roulette#playforreal",
-    "Roulette VIP": "https://www.32red.com/play/roulette-vip#playforreal",
-    "Double Ball Roulette": "https://www.32red.com/play/double-ball-roulette#playforreal",
-}
 
